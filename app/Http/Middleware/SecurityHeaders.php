3dac0734a81082fc1d3a032a9ada7c4b2658ea4d
Benjamin Lau <capeguy@capeguy.net>
2024-07-09 20:05:42 +0800

    Update CSP

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -88,13 +88,12 @@ class SecurityHeaders
             $csp_policy[] = "connect-src 'self'";
             $csp_policy[] = "object-src 'none'";
             $csp_policy[] = "font-src 'self' data:";
-            $csp_policy[] = "img-src 'self' data: ".config('app.url').' '.env('PUBLIC_AWS_URL').' https://secure.gravatar.com http://gravatar.com maps.google.com maps.gstatic.com *.googleapis.com';
+            // $csp_policy[] = "img-src 'self' data: ".config('app.url').' '.env('PUBLIC_AWS_URL').' https://secure.gravatar.com http://gravatar.com maps.google.com maps.gstatic.com *.googleapis.com';
 	          
             if (config('filesystems.disks.public.driver') == 's3') {
                $csp_policy[] = "img-src 'self' data:  ".config('filesystems.disks.public.url');
-            } else {
-                $csp_policy[] = "img-src * 'self' data: https:";
             }
+            $csp_policy[] = "img-src * 'self' data: https:";
             
             $csp_policy = join(';', $csp_policy);
            
