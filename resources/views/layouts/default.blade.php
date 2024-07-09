2bd3c5d08ac2efcda5094b365baac041cf646ca7
Benjamin Lau <capeguy@capeguy.net>
2024-07-09 20:02:19 +0800

    sigh

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -33,7 +33,7 @@ dir="{{ in_array(app()->getLocale(),['ar-SA','fa-IR', 'he-IL']) ? 'rtl' : 'ltr' 
 
     {{-- stylesheets --}}
     <link rel="stylesheet" href="{{ url(mix('css/dist/all.css')) }}">
-    <link rel="stylesheet" href="{{ url(mix('css/dist/bootstrap-icon.css')) }}">
+    <link rel="stylesheet" href="{{ url(mix('css/dist/bootstrap-icons.css')) }}">
     @if (($snipeSettings) && ($snipeSettings->allow_user_skin==1) && Auth::check() && Auth::user()->present()->skin != '')
         <link rel="stylesheet" href="{{ url(mix('css/dist/skins/skin-'.Auth::user()->present()->skin.'.min.css')) }}">
     @else
@@ -1167,6 +1167,7 @@ dir="{{ in_array(app()->getLocale(),['ar-SA','fa-IR', 'he-IL']) ? 'rtl' : 'ltr' 
                 .catch(err => {
                     // failure, handle it.
                     console.log(`Error scanning file.`)
+                    console.log(err);
                     alert(err);
                 });
                 });
